9c5f8612c724290cfe656f8daf1483a0ef55c469
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-03-10 19:34:13 +0100

    Add templates mustache.js to code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,45 @@
 'use-strict';
+var content = [{
+        id: 'carousel-cell1',
+        image: 'images/1.png',
+        content: 'Italy/Alps',
+        alt: 'image mountain',
+    },
+    {
+        id: 'carousel-cell2',
+        image: 'images/2.png',
+        content: 'China',
+        alt: 'image china',
+    },
+    {
+        id: 'carousel-cell3',
+        image: 'images/3.png',
+        content: 'Australia',
+        alt: 'image sydney australia',
+    },
+    {
+        id: 'carousel-cell4',
+        image: 'images/4.png',
+        content: 'Island',
+        alt: 'image island',
+    }, {
+        id: 'carousel-cell5',
+        image: 'images/5.png',
+        content: 'New York',
+        alt: 'image new york',
+    },
+];
+
+var myTemplate = document.querySelector('#template').innerHTML;
+Mustache.parse(myTemplate);
+var pastedTamplates = '';
+var carousel = document.querySelector('.main-carousel');
+
+for (var i = 0; i < content.length; i++) {
+    pastedTamplates += Mustache.render(myTemplate, content[i]);
+}
+carousel.innerHTML = pastedTamplates;
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
     cellAlign: 'left',
